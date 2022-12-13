775edd1529788fd24a0089580b5b18e8fce06d4f
sris6 <srisris202@gmail.com>
2022-12-13 17:50:45 +0530

    js day 1

---
+++ script.js
@@ -0,0 +1,139 @@
+
+
+// document.getElementById("count").innerText = 5
+
+
+// let myAge = 21;
+
+// let humanDogRatio = 7;
+
+// let myDogAge= myAge * humanDogRatio ;
+
+
+// console.log (myDogAge)
+
+
+// var counter = 0 ;
+
+
+
+// function increment () {
+
+
+//     counter = counter + 1 ; 
+
+//     console.log (counter)
+
+//     document.getElementById("count").innerText = counter
+    
+//     // alert( "The button is clicked !!!")
+
+// }
+
+
+// function decrement () {
+
+
+//     counter = counter - 1 ; 
+
+//     console.log (counter)
+
+//     document.getElementById("count").innerText = counter
+    
+//     // alert( "The button is clicked !!!")
+
+// }
+
+
+
+// function save () {
+
+//     let saveStr = counter + " - " ;
+
+//     document.getElementById("save-el").innerHTML += saveStr ;
+
+//     counter = 0;
+
+//     document.getElementById("count").innerHTML = counter ;
+// }
+
+// let name = "Sri !"
+
+// let greeting = "Welcome to the JS class "
+
+// let welcomeEl = greeting + name ;
+
+// document.getElementById("welcome").innerText = welcomeEl ;
+
+
+// .........
+
+
+// Simple Calculator ... 
+
+
+
+// let num_1 = document.getElementById("num-1").innerHTML = 8
+// let num_2 = document.getElementById("num-2").innerHTML = 2
+
+// let result = document.getElementById("result")
+
+// console.log(num_1 + num_2)
+
+
+// function add () {
+//     let addResult = num_1 + num_2 ;
+//     result.innerHTML = "Result : " + addResult
+// }
+
+// function sub () {
+//     let subResult = num_1 - num_2 ;
+//     result.innerHTML = "Result : " + subResult
+// }
+
+// function div () {
+//     let divResult = num_1 / num_2 ;
+//     result.innerHTML = "Result : " + divResult 
+// }
+
+// function mul () {
+//     let mulResult = num_1 * num_2 ;
+//     result.innerHTML = "Result : " + mulResult
+// }
+
+
+// .........
+
+
+// let age = "20" ;
+
+// if (age == 20){
+//     console.log ("Bla Bla Blaaaa")
+// }
+// else if (age == 20) {
+//     console.log ("HAkuna matata")
+// }
+
+// let num1 = 10
+// let num2 = 11
+// let total = num1 + num2 
+
+
+// let messageEl = document.getElementById("message");
+
+
+// function message () {
+//     if(total < 21) {
+//         message = "First condition !"
+//     }
+
+//     else if (total === 21) {
+//         message = "Second condition !"
+//     }
+
+//     else {
+//         message = "Last condition ! "
+//     }
+
+//     messageEl.innerHTML = message ;
+// }
